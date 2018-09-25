26c89bccb1ab1a04bdcf54bfca39bf5f17b7b45d
cuiyongming <624848668@qq.com>
2018-09-26 06:49:19 +0800

    基于baseline，修改loss为focalLoss，精度提升比较明显

--- main_inception_v4.py
+++ main_inception_v4.py
@@ -299,16 +299,18 @@ def main():
         else:
             final_acc = PRED_CORRECT_COUNT / PRED_COUNT
         return final_acc * 100, PRED_COUNT
-    
+
+
+
     # 程序主体
 
     # GPU ID
-    os.environ["CUDA_VISIBLE_DEVICES"] = '0, 1'
-    batch_size = 24
-    workers = 12
+    os.environ["CUDA_VISIBLE_DEVICES"] = '0'
+    batch_size = 10
+    workers = 0
 
     # epoch数量，分stage进行，跑完一个stage后降低学习率进入下一个stage
-    stage_epochs = [20, 10, 10]  
+    stage_epochs = [20, 10, 10]
     # 初始学习率
     lr = 1e-4
     # 学习率衰减系数 (new_lr = lr / lr_decay)
@@ -402,8 +404,75 @@ def main():
     val_loader = DataLoader(val_data, batch_size=batch_size*2, shuffle=False, pin_memory=False, num_workers=workers)
     test_loader = DataLoader(test_data, batch_size=batch_size*2, shuffle=False, pin_memory=False, num_workers=workers)
 
+    from torch.autograd import Variable
+    import torch.nn.functional as F
+
+    class FocalLoss(nn.Module):
+        r"""
+            This criterion is a implemenation of Focal Loss, which is proposed in
+            Focal Loss for Dense Object Detection.
+
+                Loss(x, class) = - \alpha (1-softmax(x)[class])^gamma \log(softmax(x)[class])
+
+            The losses are averaged across observations for each minibatch.
+
+            Args:
+                alpha(1D Tensor, Variable) : the scalar factor for this criterion
+                gamma(float, double) : gamma > 0; reduces the relative loss for well-classiﬁed examples (p > .5),
+                                       putting more focus on hard, misclassiﬁed examples
+                size_average(bool): By default, the losses are averaged over observations for each minibatch.
+                                    However, if the field size_average is set to False, the losses are
+                                    instead summed for each minibatch.
+
+
+        """
+
+        def __init__(self, class_num, alpha=None, gamma=2, size_average=True):
+            super(FocalLoss, self).__init__()
+            if alpha is None:
+                self.alpha = Variable(torch.ones(class_num, 1))
+            else:
+                if isinstance(alpha, Variable):
+                    self.alpha = alpha
+                else:
+                    self.alpha = Variable(alpha)
+            self.gamma = gamma
+            self.class_num = class_num
+            self.size_average = size_average
+
+        def forward(self, inputs, targets):
+            N = inputs.size(0)
+            C = inputs.size(1)
+            P = F.softmax(inputs)
+
+            class_mask = inputs.data.new(N, C).fill_(0)
+            class_mask = Variable(class_mask)
+            ids = targets.view(-1, 1)
+            class_mask.scatter_(1, ids.data, 1.)
+            # print(class_mask)
+
+            if inputs.is_cuda and not self.alpha.is_cuda:
+                self.alpha = self.alpha.cuda()
+            alpha = self.alpha[ids.data.view(-1)]
+
+            probs = (P * class_mask).sum(1).view(-1, 1)
+
+            log_p = probs.log()
+            # print('probs size= {}'.format(probs.size()))
+            # print(probs)
+
+            batch_loss = -alpha * (torch.pow((1 - probs), self.gamma)) * log_p
+            # print('-----bacth_loss------')
+            # print(batch_loss)
+
+            if self.size_average:
+                loss = batch_loss.mean()
+            else:
+                loss = batch_loss.sum()
+            return loss
     # 使用交叉熵损失函数
-    criterion = nn.CrossEntropyLoss().cuda()
+    #criterion = nn.CrossEntropyLoss().cuda()
+    criterion = FocalLoss(class_num=12).cuda()
 
     # 优化器，使用带amsgrad的Adam
     optimizer = optim.Adam(model.parameters(), lr, weight_decay=weight_decay, amsgrad=True)
